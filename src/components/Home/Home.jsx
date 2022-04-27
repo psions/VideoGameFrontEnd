9b3ba8c772eeb83fc1243973019dfefc386f2d5a
psions <psionreality@gmail.com>
2022-04-27 18:24:39 -0500

    created widgets for graph and featured console

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,8 @@ import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
 import "./home.scss"
 import Widget from "../../components/widget/Widget"
+import Featured from "../../components/featured/Featured"
+import Chart from "../../components/chart/Chart"
 
 
 const Home = () => {
@@ -14,6 +16,10 @@ const Home = () => {
                     <Widget type="game"/>
                     <Widget type="console"/>
                     <Widget type="statistics"/>
+                </div>
+                <div className="charts">
+                    <Featured/>
+                    <Chart/>
                 </div>
             </div>
         </div>
