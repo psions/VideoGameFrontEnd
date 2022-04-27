620515d8493df2289016e220f4ab3a480323c807
psions <psionreality@gmail.com>
2022-04-27 17:45:29 -0500

    added color outlines to widgets

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -17,23 +17,47 @@ const Widget = ({type}) => {
                 title: "Games",
                 isMoney: false,
                 link:"See all games",
-                icon: <PersonOutlineOutlinedIcon className="icon" />,
+                icon: (
+                    <PersonOutlineOutlinedIcon 
+                    className="icon" 
+                    style={{
+                        color: "crimson",
+                        backgroundColor: "rgba(255, 0, 0, 0.2)",
+                    }}
+                    />
+                ),
             };
             break;
-            case "console":
+        case "console":
             data={
                 title: "CONSOLES",
                 isMoney: false,
                 link:"See all consoles",
-                icon: <VideogameAssetIcon className="icon" />,
+                icon: (
+                    <VideogameAssetIcon 
+                    className="icon"
+                    style={{
+                        backgroundColor: "rgba(128, 0, 128, 0.2) ",
+                        color: "purple",
+                    }}
+                    />
+                ),
             };
             break;
-            case "statistics":
+        case "statistics":
             data={
                 title: "Stats",
                 isMoney: true,
                 link:"See all stats",
-                icon: <AnalyticsIcon className="icon" />,
+                icon:( 
+                    <AnalyticsIcon 
+                    className="icon"
+                    style={{
+                        backgroundColor: "rgba(218, 165, 32, 0.2",
+                        color: "goldenrod",
+                    }}
+                />
+                ),
             };
             break;
         default:
